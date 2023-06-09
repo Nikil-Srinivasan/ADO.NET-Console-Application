8b833a66429579a96f2832457abf054b66112c12
Nikil-Srinivasan <nikil.s.cce@gmail.com>
2023-06-09 10:05:32 +0530

    Handled specific exceptions

--- Program.cs
+++ Program.cs
@@ -13,11 +13,12 @@ class Program
             // Creating Connection  
             connection = new SqlConnection("data source=.; database=office; integrated security=SSPI");
             connection.Open();
+            AnsiConsole.Write(new FigletText("Connection Established").LeftJustified().Color(Color.Green));
 
             while (isRunning)
             {
-                Console.Write("\n~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~");
-                Console.WriteLine("\nEnter the Choice to perform an action:");
+                Console.WriteLine("\n~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~");
+                Console.WriteLine("Enter the Choice to perform an action:");
                 Console.WriteLine("~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~");
                 Console.WriteLine("1. Create Table");
                 Console.WriteLine("2. Insert Data");
@@ -27,13 +28,13 @@ class Program
                 Console.WriteLine("6. Display Available Tables");
                 Console.WriteLine("7. Close Application");
                 Console.Write("\nYour Choice: ");
-                int choice = Convert.ToInt32(Console.ReadLine());
+                string choice = Console.ReadLine();
 
                 SqlCommand command;
 
                 switch (choice)
                 {
-                    case 1:
+                    case "1":
                         // Create Table
                         Console.Write("\nEnter table name: ");
                         string tableNameCreate = Console.ReadLine();
@@ -44,7 +45,7 @@ class Program
                         string columnDetails = "";
                         for (int i = 0; i < columnCount; i++)
                         {
-                            Console.Write($"Enter column {i + 1} name: ");
+                            Console.Write($"\nEnter column {i + 1} name: ");
                             string columnName = Console.ReadLine();
                             Console.Write($"Enter {columnName} type: ");
                             string columnType = Console.ReadLine();
@@ -62,87 +63,127 @@ class Program
                                 columnDetails += ", ";
                         }
 
-                        command = new SqlCommand($"CREATE TABLE {tableNameCreate} ({columnDetails})", connection);
-                        command.ExecuteNonQuery();
-                        Console.WriteLine("\n%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%");
-                        Console.WriteLine("|| Table created successfully ||");
-                        Console.WriteLine("%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%\n");
+                        try
+                        {
+                            command = new SqlCommand($"CREATE TABLE {tableNameCreate} ({columnDetails})", connection);
+                            command.ExecuteNonQuery();
+                            Console.WriteLine("\n%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%");
+                            Console.WriteLine("|| Table created successfully ||");
+                            Console.WriteLine("%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%\n");
+                        }
+                        catch (SqlException)
+                        {
+                            Console.WriteLine("\n%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%");
+                            Console.WriteLine("|| Invalid data type. Table creation failed. ||");
+                            Console.WriteLine("%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%\n");
+                        }
                         break;
 
-                    case 2:
+                    case "2":
                         // Insert Data
                         Console.Write("\nEnter table name: ");
                         string tableNameInsert = Console.ReadLine();
-                        command = new SqlCommand($"SELECT * FROM {tableNameInsert}", connection);
-                        SqlDataReader insertReader = command.ExecuteReader();
-
-                        // Collecting column names
-                        var columnNamesInsert = new List<string>();
-                        for (int i = 0; i < insertReader.FieldCount; i++)
+                        try
                         {
-                            columnNamesInsert.Add(insertReader.GetName(i));
-                        }
-                        insertReader.Close();
+                            command = new SqlCommand($"SELECT * FROM {tableNameInsert}", connection);
+                            SqlDataReader insertReader = command.ExecuteReader();
 
-                        // Collecting values for each column
-                        var columnValuesInsert = new List<string>();
-                        foreach (var columnName in columnNamesInsert)
+                            // Collecting column names
+                            var columnNamesInsert = new List<string>();
+                            for (int i = 0; i < insertReader.FieldCount; i++)
+                            {
+                                columnNamesInsert.Add(insertReader.GetName(i));
+                            }
+                            insertReader.Close();
+
+                            // Collecting values for each column
+                            var columnValuesInsert = new List<string>();
+                            foreach (var columnName in columnNamesInsert)
+                            {
+                                Console.Write($"Enter value for column '{columnName}': ");
+                                string columnValue = Console.ReadLine();
+                                columnValuesInsert.Add(columnValue);
+                            }
+
+                            // Generating SQL query dynamically
+                            string insertQuery = $"INSERT INTO {tableNameInsert} ({string.Join(", ", columnNamesInsert)}) VALUES ('{string.Join("', '", columnValuesInsert)}')";
+                            // INSERT INTO tbl_name (col_name_1, col_name_2, ..., col_name_n) VALUES (value_1, value_2, ... value_n)
+
+                            command = new SqlCommand(insertQuery, connection);
+                            command.ExecuteNonQuery();
+                            Console.WriteLine("\n%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%");
+                            Console.WriteLine("|| Values inserted successfully ||");
+                            Console.WriteLine("%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%\n");
+                        }
+                        catch (SqlException ex)
                         {
-                            Console.Write($"Enter value for column '{columnName}': ");
-                            string columnValue = Console.ReadLine();
-                            columnValuesInsert.Add(columnValue);
+                            if (ex.Number == 208)
+                            {
+                                Console.WriteLine("\n%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%");
+                                Console.WriteLine("|| Invalid table name. Insertion failed. ||");
+                                Console.WriteLine("%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%\n");
+                            }
+                            else
+                            {
+                                Console.WriteLine("\n%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%");
+                                Console.WriteLine("|| Invalid data. Insertion failed ||");
+                                Console.WriteLine("%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%\n");
+                            }
                         }
-
-                        // Generating SQL query dynamically
-                        string insertQuery = $"INSERT INTO {tableNameInsert} ({string.Join(", ", columnNamesInsert)}) VALUES ('{string.Join("', '", columnValuesInsert)}')";
-                        // INSERT INTO tbl_name (col_name_1, col_name_2, ..., col_name_n) VALUES (value_1, value_2, ... value_n)
-                        command = new SqlCommand(insertQuery, connection);
-                        command.ExecuteNonQuery();
-                        Console.WriteLine("\n%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%");
-                        Console.WriteLine("|| Values inserted successfully ||");
-                        Console.WriteLine("%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%\n");
                         break;
 
-                    case 3:
+                    case "3":
                         // Read Table
                         Console.Write("\nEnter table name: ");
                         string tableNameToRead = Console.ReadLine();
-                        command = new SqlCommand($"SELECT * FROM {tableNameToRead}", connection);
-                        SqlDataReader reader = command.ExecuteReader();
-
-                        // Collecting column names
-                        var columnNamesToRead = new List<string>();
-                        for (int i = 0; i < reader.FieldCount; i++)
+                        try
                         {
-                            columnNamesToRead.Add(reader.GetName(i));
-                        }
+                            command = new SqlCommand($"SELECT * FROM {tableNameToRead}", connection);
+                            SqlDataReader reader = command.ExecuteReader();
 
-                        // Formatting table details
-                        // Display column names
-                        Console.WriteLine();
-                        foreach (var columnName in columnNamesToRead)
-                        {
-                            Console.Write($"{columnName,-15}");
-                        }
-                        Console.WriteLine("\n");
+                            // Collecting column names
+                            var columnNamesToRead = new List<string>();
+                            for (int i = 0; i < reader.FieldCount; i++)
+                            {
+                                columnNamesToRead.Add(reader.GetName(i));
+                            }
 
-                        // Display column values
-                        while (reader.Read())
-                        {
+                            // Formatting table details
+                            // Display column names
+                            Console.WriteLine();
                             foreach (var columnName in columnNamesToRead)
                             {
-                                Console.Write($"{reader[columnName],-15}");
+                                Console.Write($"{columnName,-15}");
                             }
-                            Console.WriteLine();
-                        }
+                            Console.WriteLine("\n");
 
-                        reader.Close();
-                        Console.WriteLine("\n%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%");
-                        Console.WriteLine("|| Table readed successfully  ||");
-                        Console.WriteLine("%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%\n");
+                            // Display column values
+                            while (reader.Read())
+                            {
+                                foreach (var columnName in columnNamesToRead)
+                                {
+                                    Console.Write($"{reader[columnName],-15}");
+                                }
+                                Console.WriteLine();
+                            }
+
+                            reader.Close();
+                            Console.WriteLine("\n%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%");
+                            Console.WriteLine("|| Table readed successfully  ||");
+                            Console.WriteLine("%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%\n");
+                        }
+                        catch (SqlException ex)
+                        {
+                            if (ex.Number == 208)
+                            {
+                                Console.WriteLine("\n%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%");
+                                Console.WriteLine("|| Invalid table name. Insertion failed. ||");
+                                Console.WriteLine("%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%\n");
+                            }
+                        }
                         break;
 
-                    case 4:
+                    case "4":
                         // Update Data
                         Console.Write("\nEnter table name: ");
                         string tableNameUpdate = Console.ReadLine();
@@ -155,25 +196,71 @@ class Program
                         Console.Write($"Enter new value for '{updateColumn}': ");
                         string updateValue = Console.ReadLine();
 
-                        command = new SqlCommand($"UPDATE {tableNameUpdate} SET {updateColumn} = '{updateValue}' WHERE {conditionColumn} = '{conditionValue}'", connection);
-                        command.ExecuteNonQuery();
-                        Console.WriteLine("\n%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%");
-                        Console.WriteLine("|| Table updated successfully ||");
-                        Console.WriteLine("%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%\n");
+                        try
+                        {
+                            command = new SqlCommand($"UPDATE {tableNameUpdate} SET {updateColumn} = '{updateValue}' WHERE {conditionColumn} = '{conditionValue}'", connection);
+                            int rowsAffected = command.ExecuteNonQuery();
+
+                            if (rowsAffected > 0)
+                            {
+                                Console.WriteLine("\n%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%");
+                                Console.WriteLine("|| Table updated successfully ||");
+                                Console.WriteLine("%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%\n");
+                            }
+                            else
+                            {
+                                Console.WriteLine("\n%%%%%%%%%%%%%%%%%%%%%%%%%");
+                                Console.WriteLine("|| Table update failed ||");
+                                Console.WriteLine("%%%%%%%%%%%%%%%%%%%%%%%%%\n");
+                            }
+                        }
+                        catch (SqlException ex)
+                        {
+                            if (ex.Number == 208)
+                            {
+                                Console.WriteLine("\n%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%");
+                                Console.WriteLine("|| Invalid table name. Table update failed ||");
+                                Console.WriteLine("%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%\n");
+                            }
+                            else if (ex.Number == 207)
+                            {
+                                Console.WriteLine("\n%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%");
+                                Console.WriteLine("|| Invalid column name. Table update failed ||");
+                                Console.WriteLine("%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%\n");
+                            }
+                            else
+                            {
+                                Console.WriteLine("\n%%%%%%%%%%%%%%%%%%%%%%%%%%");
+                                Console.WriteLine("|| Table update failed. ||");
+                                Console.WriteLine("%%%%%%%%%%%%%%%%%%%%%%%%%%\n");
+                            }
+                        }
                         break;
 
-                    case 5:
+                    case "5":
                         // Delete Table
                         Console.Write("\nEnter table name: ");
                         string tableNameDelete = Console.ReadLine();
-                        command = new SqlCommand($"DROP TABLE IF EXISTS {tableNameDelete}", connection);
-                        command.ExecuteNonQuery();
-                        Console.WriteLine("\n%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%");
-                        Console.WriteLine("|| Table deleted successfully ||");
-                        Console.WriteLine("%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%\n");
+                        try
+                        {
+                            command = new SqlCommand($"DROP TABLE {tableNameDelete}", connection);
+                            command.ExecuteNonQuery();
+                            Console.WriteLine("\n%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%");
+                            Console.WriteLine("|| Table deleted successfully ||");
+                            Console.WriteLine("%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%\n");
+                        }
+                        catch (SqlException ex)
+                        {
+                            if (ex.Number == 3701)
+                            {
+                                Console.WriteLine("\n%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%");
+                                Console.WriteLine("|| Table does not exist in database ||");
+                                Console.WriteLine("%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%\n");
+                            }
+                        }
                         break;
 
-                    case 6:
+                    case "6":
                         // Display Available Tables
                         command = new SqlCommand("SELECT TABLE_NAME FROM INFORMATION_SCHEMA.TABLES WHERE TABLE_TYPE='BASE TABLE'", connection);
                         SqlDataReader tablesReader = command.ExecuteReader();
@@ -188,22 +275,24 @@ class Program
                         tablesReader.Close();
                         break;
 
-                    case 7:
+                    case "7":
                         // Close Application
                         isRunning = false;
                         break;
 
                     default:
-                        Console.WriteLine("Invalid choice. Please try again.");
+                        Console.WriteLine("\n%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%");
+                        Console.WriteLine("|| Invalid choice. Please try again. ||");
+                        Console.WriteLine("%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%\n");
                         break;
                 }
                 // Delay before displaying choices again
                 Thread.Sleep(2000);
             }
         }
-        catch (Exception e)
+        catch (Exception)
         {
-            Console.WriteLine("OOPs, something went wrong: " + e.Message);
+            Console.WriteLine("OOPs, something went wrong: ");
         }
         finally
         {
@@ -211,7 +300,7 @@ class Program
             if (connection != null)
             {
                 connection.Close();
-                AnsiConsole.Write(new FigletText("Application Closed...").LeftJustified().Color(Color.DarkOrange));
+                AnsiConsole.Write(new FigletText("Connection Closed...").LeftJustified().Color(Color.Red));
             }
 
         }
